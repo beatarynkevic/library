a6d808d3ff9914c730452053f5cfbb8b49ce8a4a
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-03-24 16:04:31 +0200

    +

--- resources/views/author/index.blade.php
+++ resources/views/author/index.blade.php
@@ -1,7 +1,8 @@
 @foreach ($authors as $author)
-{{$author->name}} {{$author->surname}}<br>
+<a href="{{route('author.edit',[$author])}}">{{$author->name}} {{$author->surname}}</a>
+<form method="POST" action="{{route('author.destroy', [$author])}}">
+    @csrf
+    <button type="submit">DELETE</button>
+</form>
+<br>
 @endforeach
-{{--
-@foreach ($authors as $author)
-<a href="{{route('author.edit',[$author])}}">{{$author->name}} {{$author->surname}}</a><br>
-@endforeach --}}
