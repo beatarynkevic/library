85c8496c93d6acb1501568519b59ef2358e79632
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-04-01 17:26:09 +0300

    +

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -100,7 +100,8 @@ class BookController extends Controller
     public function edit(Book $book)
     {
         $authors = Author::all();
-        return view('book.edit', ['book' => $book, 'authors' => $authors]);
+        $publishers = Publisher::all();
+        return view('book.edit', ['book' => $book, 'authors' => $authors, 'publishers' => $publishers]);
     }
 
     /**
